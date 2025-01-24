45b729f8aa7957d034d81133421e87ce29c8bdf1
Max (Lenovo) <max.murphy11@gmail.com>
2025-01-24 14:50:52 -0500

    Improves help command formatting, information
    Can request detailed help for individual commands using syntax `tactor('h', '<command>');`.

--- src/tactor.cpp
+++ src/tactor.cpp
@@ -59,6 +59,25 @@ static std::map<int, std::string> errorDescriptions = {
     {602000, "Bad data."}
 };
 
+static std::map<std::string, uint8_t> stringCommands = {
+    {"initialize", 1},
+    {"shutdown", 2},
+    {"discover", 3},
+    {"getName", 4},
+    {"connect", 5},
+    {"setTimeFactor", 6},
+    {"changeGain", 7},
+    {"changeFreq", 8},
+    {"rampGain", 9},
+    {"rampFreq", 10},
+    {"pulse", 11},
+    {"stop", 12},
+    {"setState", 13},
+    {"beginStoreTAction", 14},
+    {"finishStoreTAction", 15},
+    {"playStoredTAction", 16}
+};
+
 // Function to get error description
 const char* getErrorDescription(int errorCode) {
     auto it = errorDescriptions.find(errorCode);
@@ -66,6 +85,15 @@ const char* getErrorDescription(int errorCode) {
         return it->second.c_str();
     }
     return "Unknown error code.";
+}
+
+// Function to convert string command to uint8_t
+uint8_t stringCommandToCode(const char* command) {
+    auto it = stringCommands.find(command);
+    if (it != stringCommands.end()) {
+        return it->second;
+    }
+    return 0;
 }
 
 // Cleanup function for when MATLAB exits
@@ -82,60 +110,230 @@ void handleError(int result, const char* functionName) {
     if (result < 0) {
         int errorCode = GetLastEAIError();
         const char* description = getErrorDescription(errorCode);
-        mexErrMsgIdAndTxt("TDK:Error", "%s failed with error code: %d (%s)", functionName, errorCode, description);
+        mexErrMsgIdAndTxt("TDK:Error", "<strong>%s</strong> failed with error code: %d\n\t->\t(%s)", functionName, errorCode, description);
     }
 }
 
-// Help function
-void printHelp() {
-    mexPrintf("TDK Vibrotactor MEX Interface Help:\n");
-    mexPrintf("----------------------------------\n");
-    mexPrintf("Usage: tdk.tactor(<command>, <args>...)\n\n");
-    mexPrintf("Commands:\n");
-    mexPrintf("  'initialize'            Initialize the tactor interface.\n");
-    mexPrintf("  'shutdown'              Shutdown the tactor interface and clean up resources.\n");
-    mexPrintf("  'discover', <type>      Discover devices of the specified type (e.g., USB = 1).\n");
-    mexPrintf("  'connect', <name>, <type>\n");
-    mexPrintf("                          Connect to a device with the given name and type.\n");
-    mexPrintf("  'setTimeFactor', <value>\n");
-    mexPrintf("                          Set the time factor for the tactor interface (1 - 255).\n");
-    mexPrintf("  'changeGain', <deviceID>, <tactor>, <gain>, <delay>\n");
-    mexPrintf("                          Change the gain of a tactor (1-indexed).\n");
-    mexPrintf("  'changeFreq', <deviceID>, <tactor>, <freq>, <delay>\n");
-    mexPrintf("                          Change the frequency (300 - 3550) of a tactor.\n");
-    mexPrintf("  'getName', <index>\n");
-    mexPrintf("                          Get the name of the tactor from (0-indexed) discovered device list.\n");
-    mexPrintf("  'rampGain', <deviceID>, <tactor>, <startGain>, <endGain>, <duration>, <delay>\n");
-    mexPrintf("                          Set linear gain ramp over some period of time and delay.\n");
-    mexPrintf("  'rampFreq', <deviceID>, <tactor>, <startFreq>, <endFreq>, <duration>, <delay>\n");
-    mexPrintf("                          Set linear frequency ramp over some period of time and delay.\n");
-    mexPrintf("  'pulse', <deviceID>, <tactor>, <duration>, <delay>\n");
-    mexPrintf("                          Pulse a tactor (1-indexed) for the specified duration and delay.\n");
-    mexPrintf("  'stop', <deviceID>, <delay>\n");
-    mexPrintf("                          Stops all tactors after the specified delay duration.\n");
-    mexPrintf("  'setState', <deviceID>, <delay>, <states>\n");
-    mexPrintf("                        Set the state of all tactors after the specified delay duration.\n");
-    mexPrintf("  'beginStoreTAction', <deviceID>, <tacID>\n");
-    mexPrintf("                          Store a TAction with specified tacID (1 - 10). Should always be called with finishStoreTAction.\n");
-    mexPrintf("  'finishStoreTAction', <deviceID>\n");
-    mexPrintf("                          Stop storing the current TAction.\n");
-    mexPrintf("  'playStoredTAction', <deviceID>, <delay>, <tacID>\n");
-    mexPrintf("                          Play the specified TAction after some delay.\n");
-    mexPrintf("Alternative: Use uint8 codes for commands to reduce processing overhead:\n");
-    mexPrintf("  1 = 'initialize', 2 = 'shutdown', 3 = 'discover'\n");
-    mexPrintf("  4 = 'getName', 5 = 'connect'\n"); 
-    mexPrintf("  6 = 'setTimeFactor', 7 = 'changeGain', 8 = 'changeFreq'\n"); 
-    mexPrintf("  9 = 'rampGain', 10 = 'rampFreq'\n");
-    mexPrintf("  11 = 'pulse', 12 = 'stop', 13 = 'setState', \n");
-    mexPrintf("  14 = 'beginStoreTAction', 15 = 'finishStoreTAction', 16 = 'playStoredTACtion'\n\n");
-    mexPrintf("Examples:\n");
+void printHelpCodeList() {
+    mexPrintf("<strong>uint8 function equivalents</strong>:\n");
+    mexPrintf("  1 = 'initialize'\n");
+    mexPrintf("  2 = 'shutdown'\n");
+    mexPrintf("  3 = 'discover'\n");
+    mexPrintf("  4 = 'getName'\n");
+    mexPrintf("  5 = 'connect'\n"); 
+    mexPrintf("  6 = 'setTimeFactor'\n"); 
+    mexPrintf("  7 = 'changeGain'\n");
+    mexPrintf("  8 = 'changeFreq'\n"); 
+    mexPrintf("  9 = 'rampGain'\n"); 
+    mexPrintf("  10 = 'rampFreq'\n");
+    mexPrintf("  11 = 'pulse'\n");
+    mexPrintf("  12 = 'stop'\n");
+    mexPrintf("  13 = 'setState'\n");
+    mexPrintf("  14 = 'beginStoreTAction'\n");
+    mexPrintf("  15 = 'finishStoreTAction'\n");
+    mexPrintf("  16 = 'playStoredTACtion'\n\n");
+}
+
+void printHelpCommandDetails(uint8_t command, bool detailed) {
+    if (command == 0) {
+        for (int i = 1; i <= 16; i++) {
+            printHelpCommandDetails(i, false);
+        }
+        mexPrintf("\n<strong>General</strong>\n");
+        mexPrintf("  'h'                    Print default help.\n");
+        mexPrintf("  'l'                    Print list of all valid command names.\n");
+        mexPrintf("  'h', <command>         Get help for a specific command.\n\n");
+        return;
+    }
+    if (detailed) {
+        mexPrintf("Usage: tactor(<command>, <args>...)\n");
+    }
+    switch (command) {
+        case 1:
+            mexPrintf("  'initialize'           Initialize the tactor interface.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Note:</strong> First call before any others.\n");
+                mexPrintf("                                                      (handled by tdk.open())\n");
+                mexPrintf("                                     See also: tdk.open()\n");
+            }
+            break;
+        case 2:
+            mexPrintf("  'shutdown'             Shutdown the tactor interface and clean up resources.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Note:</strong> Must be called before exiting MATLAB.\n");
+                mexPrintf("                                     See also: tdk.close()\n");
+            }
+            break;
+        case 3:
+            mexPrintf("  'discover', <type>\n");
+            mexPrintf("                         Discover devices of the specified type (e.g., USB = 1).\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Returns:</strong> number of discovered devices.\n");
+                mexPrintf("                          -> Note: must be called before 'connect'.\n");
+                mexPrintf("                                            (handled by tdk.open())\n");
+                mexPrintf("                                     See also: tdk.open()\n");
+            }
+            break;
+        case 4:
+            mexPrintf("  'getName', <index>\n");
+            mexPrintf("                         Get the name of the tactor from (0-indexed) discovered device list.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Returns:</strong> device name as a string (e.g. 'COM9').\n");
+                mexPrintf("                                            (handled by tdk.open())\n");
+                mexPrintf("                                               See also: tdk.open()\n");
+            }
+            
+            break;
+        case 5:
+            mexPrintf("  'connect', <name>, <type>\n");
+            mexPrintf("                         Connect to a device with the given name and type.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Returns:</strong> `deviceID` of connected device (integer e.g. 0).\n");
+                mexPrintf("                                            (handled by tdk.open())\n");
+                mexPrintf("                                               See also: tdk.open()\n");
+            }
+            break;
+        case 6:
+            mexPrintf("  'setTimeFactor', <value>\n");
+            mexPrintf("                         Set the time factor for the tactor interface (1 - 255).\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Does not appear to work.</strong>\n");
+            }
+            break;
+        case 7:
+            mexPrintf("  'changeGain', <deviceID>, <tactor>, <gain>, <delay>\n");
+            mexPrintf("                         Change the gain of a tactor (1-indexed).\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                        IN: <strong>deviceID</strong> - The device ID to apply the command to.\n");
+                mexPrintf("                        IN: <strong>tactor</strong> - The tactor number for the command. 1-indexed.\n");
+                mexPrintf("                        IN: <strong>gain</strong> - The gain value (1 - 255).\n");
+                mexPrintf("                        IN: <strong>delay</strong> - Delay before running command (ms).\n");
+                mexPrintf("                                                     Does not seem to do anything.\n");
+            }
+            break;
+        case 8:
+            mexPrintf("  'changeFreq', <deviceID>, <tactor>, <freq>, <delay>\n");
+            mexPrintf("                         Change the frequency (300 Hz - 3550 Hz) of a tactor.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                        IN: <strong>deviceID</strong> - The device ID to apply the command to.\n");
+                mexPrintf("                        IN: <strong>tactor</strong> - The tactor number for the command. 1-indexed.\n");
+                mexPrintf("                        IN: <strong>freq</strong> - The new frequency (Hz; 300 - 3550).\n");
+                mexPrintf("                        IN: <strong>delay</strong> - Delay before running command (ms).\n");
+                mexPrintf("                                                     Does not seem to do anything.\n");
+            }
+            break;
+        case 9:   
+            mexPrintf("  'rampGain', <deviceID>, <tactor>, <startGain>, <endGain>, <duration>, <delay>\n");
+            mexPrintf("                         Set linear gain ramp over some period of time and delay.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Does not appear to work.</strong>\n");
+            }
+            break;          
+        case 10:
+            mexPrintf("  'rampFreq', <deviceID>, <tactor>, <startFreq>, <endFreq>, <duration>, <delay>\n");
+            mexPrintf("                         Set linear frequency ramp over some period of time and delay.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Does not appear to work.</strong>\n");
+            }
+            break;
+        case 11:
+            mexPrintf("  'pulse', <deviceID>, <tactor>, <duration>, <delay>\n");
+            mexPrintf("                         Pulse a tactor (1-indexed) for the specified duration and delay.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                        IN: <strong>deviceID</strong> - The device ID to apply the command to.\n");
+                mexPrintf("                        IN: <strong>tactor</strong> - The tactor number for the command. 1-indexed.\n");
+                mexPrintf("                        IN: <strong>duration</strong> - Duration of the command (ms); range is 1-255.\n");
+                mexPrintf("                                                     Does not seem affected by `setTimeFactor` scalar.\n");
+                mexPrintf("                        IN: <strong>delay</strong> - Delay before running command (ms).\n");
+                mexPrintf("                                                     Does not seem to do anything.\n");
+            }
+            break;
+        case 12:
+            mexPrintf("  'stop', <deviceID>, <delay>\n");
+            mexPrintf("                         Stops all tactors after the specified delay duration.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                        IN: <strong>deviceID</strong> - The device ID to apply the command to.\n");
+                mexPrintf("                        IN: <strong>delay</strong> - Delay before running command (ms).\n");
+                mexPrintf("                                                     Does not seem to do anything.\n");
+            }
+            break;
+        case 13:
+            mexPrintf("  'setState', <deviceID>, <delay>, <states>\n");
+            mexPrintf("                         Set the state of all tactors after the specified delay duration.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Do not use! Seems to break MATLAB interface!</strong>\n");
+            }
+            break;
+        case 14:
+            mexPrintf("  'beginStoreTAction', <deviceID>, <tacID>\n");
+            mexPrintf("                         Store a TAction with specified tacID (1 - 10).\n");
+            mexPrintf("                         Should always be called with finishStoreTAction.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Does not appear to work.</strong>\n");
+            }
+            break;
+        case 15:
+            mexPrintf("  'finishStoreTAction', <deviceID>\n");
+            mexPrintf("                         Stop storing the current TAction.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Does not appear to work.</strong>\n");
+            }
+            break;
+        case 16:
+            mexPrintf("  'playStoredTAction', <deviceID>, <delay>, <tacID>\n");
+            mexPrintf("                         Play the specified TAction after some delay.\n");
+            if (detailed) {
+                mexPrintf("\n");
+                mexPrintf("                         <strong>Does not appear to work.</strong>\n");
+            }
+            break;
+        default:
+            mexErrMsgIdAndTxt("TDK:UnknownCommand", "Unknown command code: %d", command);
+    }
+    if (detailed) {
+        mexPrintf("\n");
+    }
+}
+
+void printHelpExamples() {
+    mexPrintf("<strong>Examples</strong>\n");
     mexPrintf("  tactor('initialize');\n");
     mexPrintf("  tactor('discover', 1);\n");
     mexPrintf("  tactor('connect', 'DeviceName', 1);\n");
     mexPrintf("  tactor('pulse', deviceID, 1, 100, 0);\n");
     mexPrintf("  tactor('shutdown');\n\n");
-    mexPrintf("Note 1: It is probably easiest to use the tdk package functions rather than tactor directly.\n");
-    mexPrintf("Note 2: As of 2025-01-24, Max has not figured out how to make TAction or the ramp functions work.\n\n");
+}
+
+void printHelpNotes() {
+    mexPrintf("<strong>Note 1</strong>: It is probably easiest to use the tdk package functions rather than tactor directly.\n");
+    mexPrintf("<strong>Note 2</strong>: As of 2025-01-24, Max has not figured out how to make TAction or the ramp functions work.\n\n");
+}
+
+// Help function
+void printHelp() {
+    mexPrintf("<strong>NML-TDK Vibrotactor MEX Interface</strong>\n");
+    mexPrintf("-----------------------------------\n");
+    mexPrintf("Usage: tactor(<command>, <args>...)\n\n");
+    mexPrintf("<strong>Commands</strong>\n");
+    printHelpCommandDetails(0, false);
+    printHelpCodeList();
+    printHelpExamples();
+    printHelpNotes();
 }
 
 // Individual command functions
@@ -362,8 +560,20 @@ void dispatchCommand(const char* command, int nrhs, const mxArray* prhs[], mxArr
         finishStoreTAction(nrhs, prhs);
     } else if (strcmp(command, "playStoredTAction") == 0) {
         playStoredTAction(nrhs, prhs);  
-    } else if ((strcmp(command, "help") == 0) || (strcmp(command, "h") == 0)) {
+    } else if ((strcmp(command, "help") == 0) || (strcmp(command,"-help") == 0)) {
         printHelp();
+    } else if ((strcmp(command, "h") == 0) || (strcmp(command,"-h") == 0)) {
+        if (nrhs > 1) {
+            char detail[64];
+            mxGetString(prhs[1], detail, sizeof(detail));
+            uint8_t cmd = stringCommandToCode(detail);
+            printHelpCommandDetails(cmd, true);
+            if (command == 0) mexErrMsgIdAndTxt("TDK:UnknownCommand", "Unknown command: %s", detail);
+        } else {
+            printHelp();
+        }
+    } else if ((strcmp(command, "list") == 0) || (strcmp(command,"-list") == 0) || (strcmp(command,"-l") == 0) || (strcmp(command,"l") == 0)) {
+        printHelpCodeList();
     } else {
         printHelp();
         mexErrMsgIdAndTxt("TDK:UnknownCommand", "Unknown command: %s", command);
@@ -442,14 +652,14 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
     }
 
     // Dispatch based on first input type
-    if (mxIsChar(prhs[0])) {
+    if (mxIsNumeric(prhs[0]) && mxGetClassID(prhs[0]) == mxUINT8_CLASS) {
+        // Integer-based dispatch
+        uint8_t command = static_cast<uint8_t>(mxGetScalar(prhs[0]));
+        dispatchCommand(command, nrhs, prhs, plhs[0]);
+    } else if (mxIsChar(prhs[0])) {
         // String-based dispatch
         char command[64];
         mxGetString(prhs[0], command, sizeof(command));
-        dispatchCommand(command, nrhs, prhs, plhs[0]);
-    } else if (mxIsNumeric(prhs[0]) && mxGetClassID(prhs[0]) == mxUINT8_CLASS) {
-        // Integer-based dispatch
-        uint8_t command = static_cast<uint8_t>(mxGetScalar(prhs[0]));
         dispatchCommand(command, nrhs, prhs, plhs[0]);
     } else {
         mexErrMsgIdAndTxt("TDK:InputError", "First argument must be a command string or uint8.");
